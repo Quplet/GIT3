437798ef0f8caf05c01b20ed56056a8020e7e6fc
Quplet <jonathanhart1114@gmail.com>
2019-05-23 11:33:31 -0400

    Fixed code

--- manipulation.hs
+++ manipulation.hs
@@ -4,5 +4,6 @@ boustrophedon xs = boustrophedon' xs 1
 boustrophedon' (xs:ys) z = (reverse' xs z):(boustrophedon' ys (z+1)) ++ []
 boustrophedon' [] z = []
 
-split (a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:xs) = a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s ++ [t] ++ split xs
-split xs = xs
+split (a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:xs) = [a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:[t]] ++ (split xs)
+split [] = []
+split xs = [xs]
